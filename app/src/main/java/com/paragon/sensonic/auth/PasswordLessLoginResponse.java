b5aa92fff93283100ce078e84c111bf42aa34cf2
rupesh <rupesh.saxena@digivalet.com>
2021-05-06 12:38:50 +0530

    done response handler for handle response and status code

--- app/src/main/java/com/paragon/sensonic/auth/PasswordLessLoginResponse.java
+++ app/src/main/java/com/paragon/sensonic/auth/PasswordLessLoginResponse.java
@@ -20,45 +20,36 @@ public class PasswordLessLoginResponse extends BaseModel<String, String> {
 
     @Override
     public void doNetworkRequest(HashMap<String, String> data, NetworkResponseCallback networkResponseCallback) {
-            AwsInterceptor awsInterceptor;
-            if (data.get("type").matches("email")) {
-                awsInterceptor = new AwsInterceptor(AppConstant.BASE_URL, HttpMethodName.POST, EndPoints.PASSWORD_LESS_LOGIN,
-                        GeneralFunctions.serialize(new EmailLoginMapper(AppConstant.SCOPE,
-                                AppConstant.BRAND_ID,
-                                AppConstant.PROPERTY_ID,
-                                data.get("email")), EmailLoginMapper.class));
-            } else {
-                awsInterceptor = new AwsInterceptor(AppConstant.BASE_URL, HttpMethodName.POST, EndPoints.PASSWORD_LESS_LOGIN,
-                        GeneralFunctions.serialize(new MobileLoginMapper(AppConstant.SCOPE,
-                                AppConstant.BRAND_ID,
-                                AppConstant.PROPERTY_ID,
-                                data.get("phone")), MobileLoginMapper.class));
+        AwsInterceptor awsInterceptor;
+        if (data.get("type").matches("email")) {
+            awsInterceptor = new AwsInterceptor(AppConstant.BASE_URL, HttpMethodName.POST, EndPoints.PASSWORD_LESS_LOGIN,
+                    GeneralFunctions.serialize(new EmailLoginMapper(AppConstant.SCOPE,
+                            AppConstant.BRAND_ID,
+                            AppConstant.PROPERTY_ID,
+                            data.get("email")), EmailLoginMapper.class));
+        } else {
+            awsInterceptor = new AwsInterceptor(AppConstant.BASE_URL, HttpMethodName.POST, EndPoints.PASSWORD_LESS_LOGIN,
+                    GeneralFunctions.serialize(new MobileLoginMapper(AppConstant.SCOPE,
+                            AppConstant.BRAND_ID,
+                            AppConstant.PROPERTY_ID,
+                            data.get("phone")), MobileLoginMapper.class));
+        }
+
+        OkHttpClient.getOkHttpClient(awsInterceptor).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                if (e instanceof NoConnectivityException) {
+                    networkResponseCallback.onInternetDisable();
+                }
+
+                networkResponseCallback.onFailure(call.toString());
             }
 
-            OkHttpClient.getOkHttpClient(awsInterceptor).enqueue(new Callback() {
-                @Override
-                public void onFailure(Call call, IOException e) {
-                    if(e instanceof NoConnectivityException){
-                        networkResponseCallback.onInternetDisable();
-                    }
-
-                    networkResponseCallback.onFailure(call.toString());
-                }
-
-                @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    switch (response.code()) {
-                        case 401:
-                        case 403:
-                            networkResponseCallback.onFailure("Invalid Token");
-                            break;
-
-                        case 200:
-                            networkResponseCallback.onResponse(GeneralFunctions.deserialize(response.body().string(), PasswordLessLogin.class));
-                            break;
-                    }
-                }
-            });
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                ResponseHandler.handleResponse(response, PasswordLessLogin.class, networkResponseCallback);
+            }
+        });
     }
 }
 
